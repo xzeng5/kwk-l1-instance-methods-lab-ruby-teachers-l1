9ce8be6932d7b196ba0411cf4b4aa68168d174b7
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-07-12 21:14:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,11 @@
 # dog.rb
 class Dog
 
-  def bark= (bark)
-    @bark = bark
-  end
-
   def bark
-    @bark
     puts "Woof!"
   end
 
-  def sit= (sit)
-    @sit = sit
-  end
-
   def sit
-    @sit
     puts "The Dog is sitting"
   end
 
