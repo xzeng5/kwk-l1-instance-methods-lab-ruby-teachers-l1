395269ad9f94ba3d8c0e36c74b55ae88fda737a2
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-07-12 21:15:06 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,21 +1,11 @@
 # person.rb
 class Person
 
-  def talk= (talk)
-    @talk = talk
-  end
-
   def talk
-    @talk
     puts "Hello World!"
   end
 
-  def walk= (walk)
-    @walk = walk
-  end
-
   def walk
-    @walk
     puts "The Person is walking"
   end
 
